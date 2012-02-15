1dfa823a1c27ca212526c35877b9b1d90fa474f0
Arthur Pan <jtp0102@gmail.com>
2012-02-15 18:00:26 +0800

    edit header

--- application/views/header.php
+++ application/views/header.php
@@ -4,8 +4,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Playd8.mobi</title>
-<link rel="stylesheet"  href="http://code.jquery.jquerycom/mobile/1.0rc1/jquery.mobile-1.0rc1.min.css" />
-<script src="httpttp://code.jquery.com/jquery-1.6.4.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
 </head>
 <body>
