ed9d6ae64a056a0e27ae6739c816e09b73b327ec
Arthur Pan <jtp0102@gmail.com>
2012-02-15 18:23:37 +0800

    edit

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Playd8.mobi</title>
+<title>FG Mobile Web</title>
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
